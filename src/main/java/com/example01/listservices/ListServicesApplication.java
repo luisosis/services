5fb5c93f696061850182f8c244e17195c71c20fa
Luis Osis <luis_14xiv@hotmail.com>
2020-01-13 09:28:20 -0500

    primer commit

---
+++ src/main/java/com/example01/listservices/ListServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example01.listservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example01/listservices/ListServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example01.listservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
